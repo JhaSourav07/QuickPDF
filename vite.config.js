cccf8ebd4493d59391b97da596fa2876c3cdf627
Sourav Jha <souravkjha2007@gmail.com>
2026-04-20 00:26:54 +0530

    config: update vite.config.js with vitest environment settings

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["pdfjs-dist"],
   },
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
 });
